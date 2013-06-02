d619440f9080c56c532812a38f8ac90d3cfabe0c
Abhin Chhabra <chhabra.abhin@gmail.com>
2013-06-02 13:32:20 -0600

    Added bower and corresponding files; added requirejs; added background sexiness

---
+++ development/media/js/require_config.js
@@ -0,0 +1,15 @@
+var require = {
+	baseUrl: '/media',
+	paths: {
+		jquery: [
+		'http://ajax.googleapis.com/ajax/libs/jquery/2.0.1/jquery.min',
+		'vendor/jquery/jquery.min'
+		]
+	},
+	config: {
+		'js/events': {
+			'sig_id': '14341068',
+			'sig': '84d878e35684af720d5170c5426a37f14e67eea6'
+		}
+	}
+};
