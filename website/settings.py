49c5e2fc6030dfc062c992c19e586d90d70d0c38
Andrew Neitsch <andrew@neitsch.ca>
2022-02-18 04:36:18 -0700

    Remove innodb_strict_mode=1
    It started triggering
    
        OperationalError: (1227, 'Access denied; you need (at least one of) the SYSTEM_VARIABLES_ADMIN or SESSION_VARIABLES_ADMIN privilege(s) for this operation')
    
    on the public deployment.

--- website/settings.py
+++ website/settings.py
@@ -133,7 +133,7 @@ DATABASES = {
         "USER": env_file["MYSQL_USER"],
         "PASSWORD": env_file["MYSQL_PASSWORD"],
         "OPTIONS": {
-            "init_command": "SET sql_mode='STRICT_ALL_TABLES', innodb_strict_mode=1",
+            "init_command": "SET sql_mode='STRICT_ALL_TABLES'",
             "charset": "utf8mb4",
         },
     }
